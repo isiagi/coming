6aa5717d163665fcc4bf7b388155e7fce91dc567
isiagi <isiagigeofrey0@gmail.com>
2025-01-01 10:23:57 +0300

    chore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,15 @@
-import './globals.css';
-
-import Navbar from '@/components/Navbar'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Navbar />
         <main>{children}</main>
       </body>
     </html>
-  )
+  );
 }
-
